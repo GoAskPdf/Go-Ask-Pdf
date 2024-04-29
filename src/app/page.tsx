ff8b9522e1b1e9303e0f3d39637abb517ed462f0
MohdShahzil59@gmail.com <9027800636@sS>
2024-04-29 18:07:47 +0530

    Overview screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
 import Navbar from "@/components/Navbar/Navbar";
+import Overview from "@/components/Overview/Overview";
 import TryItSection from "@/components/TryItSection/TryItSection";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <HeroSection />
     <div className="bottom-80 relative">
     <TryItSection />
+    <Overview />
     </div>
     </>
   );
